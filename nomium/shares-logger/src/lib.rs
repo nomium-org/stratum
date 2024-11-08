38a5fb313db05f0ded0faa63b8dd3605790ae6f5
Roman Dukhanin <romandukhanin@yandex.ru>
2024-11-08 15:09:53 +0300

    static LOGGER_SENDER Bounded channel

--- nomium/shares-logger/src/lib.rs
+++ nomium/shares-logger/src/lib.rs
@@ -1,9 +1,33 @@
 use log::info;
+use once_cell::sync::Lazy;
+use tokio::sync::mpsc;
+
+static LOGGER_SENDER: Lazy<mpsc::Sender<Vec<u8>>> = Lazy::new(|| {
+    let (tx, rx) = mpsc::channel(100); // Bounded channel with capacity 100
+    
+    // Spawn background task to process logs
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async move {
+            process_logs(rx).await;
+        });
+    });
+    
+    tx
+});
+
+async fn process_logs(mut rx: mpsc::Receiver<Vec<u8>>) {
+    while let Some(hash) = rx.recv().await {
+        info!("Share hash: {:?}", hex::encode(hash));
+    }
+}
 
 pub fn hand_shake() {
     info!("!!! SHARES-LOGGER !!!");
 }
 
 pub fn log_share(hash: Vec<u8>) {
-    info!("Share hash: {:?}", hex::encode(hash));
+    if let Err(e) = LOGGER_SENDER.try_send(hash) {
+        info!("Failed to send hash to logger: {}", e);
+    }
 }
