faf5934fe93f5dd913b1a6c0a0a97a2122dc359a
Roman R <butcher_100@mail.ru>
2024-12-08 17:02:24 +0300

    add ci-cd files

---
+++ cicd/deploy.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import os
+# Замените эти переменные на ваши значения
+PORTAINER_TOKEN = os.getenv("PORTAINER_TOKEN")
+NEXUS_URL = os.getenv("NEXUS_URL")
+CLICKHOUSE_USER = os.getenv("CLICKHOUSE_USER")
+CLICKHOUSE_PASSWORD = os.getenv("CLICKHOUSE_PASSWORD")
+PORTAINER_HOST = os.getenv("PORTAINER_HOST")
+PORTAINER_PORT = os.getenv("PORTAINER_PORT")
+STACK_ID = os.getenv("STACK_ID")
+ENDPOINT_ID = os.getenv("ENDPOINT_ID")
+
+url = f"http://{PORTAINER_HOST}:{PORTAINER_PORT}/api/stacks/{STACK_ID}/git/redeploy?endpointId={ENDPOINT_ID}"
+
+headers = {
+    "Content-Type": "application/json",
+    "X-API-Key": PORTAINER_TOKEN
+}
+
+data = {
+    "env": [
+        {"name": "CLICKHOUSE_USER", "value": CLICKHOUSE_USER},
+        {"name": "CLICKHOUSE_PASSWORD", "value": CLICKHOUSE_PASSWORD},
+        {"name": "NEXUS_URL", "value": NEXUS_URL}
+    ],
+    "prune": False,
+    "pullImage": True,
+    "repositoryAuthentication": False,
+    "repositoryReferenceName": "refs/heads/nomium-main"
+}
+
+response = requests.put(url, headers=headers, data=json.dumps(data))
+
+if response.status_code != 200:
+    print(response.text)
+    exit(2)
