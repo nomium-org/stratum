57e0c7e4c641ae00c973e3f86ade3bdf63b73a36
romandukhanin <romandukhanin@yandex.ru>
2025-04-02 19:35:09 +0300

    FIX rearrange code blocs

--- roles/translator/src/lib/proxy_config.rs
+++ roles/translator/src/lib/proxy_config.rs
@@ -23,47 +23,67 @@ pub struct ProxyConfig {
     pub upstream_difficulty_config: UpstreamDifficultyConfig,
 }
 
-fn default_upstream_address() -> String {
-    "127.0.0.1".to_string()
+pub struct UpstreamConfig {
+    pub address: String,
+    pub port: u16,
+    pub authority_pubkey: Secp256k1PublicKey,
+    pub difficulty_config: UpstreamDifficultyConfig,
 }
 
-fn default_upstream_port() -> u16 {
-    34254
-}
-
-fn default_upstream_pubkey() -> Secp256k1PublicKey {
-    "9auqWEzQDVyd2oe1JVGFLMLHZtCo2FFqZwtKA5gd9xbuEu7PH72"
-        .parse()
-        .unwrap()
-}
-
-fn default_downstream_address() -> String {
-    "0.0.0.0".to_string()
-}
-
-fn default_downstream_port() -> u16 {
-    34255
-}
-
-fn default_downstream_config() -> DownstreamDifficultyConfig {
-    DownstreamDifficultyConfig {
-        min_individual_miner_hashrate: default_min_hashrate(),
-        shares_per_minute: default_shares_per_minute(),
-        submits_since_last_update: 0,
-        timestamp_of_last_update: 0,
+impl UpstreamConfig {
+    pub fn new(
+        address: String,
+        port: u16,
+        authority_pubkey: Secp256k1PublicKey,
+        difficulty_config: UpstreamDifficultyConfig,
+    ) -> Self {
+        Self {
+            address,
+            port,
+            authority_pubkey,
+            difficulty_config,
+        }
     }
 }
 
-fn default_upstream_config() -> UpstreamDifficultyConfig {
-    UpstreamDifficultyConfig {
-        channel_diff_update_interval: default_channel_diff_update_interval(),
-        channel_nominal_hashrate: default_channel_nominal_hashrate(),
-        timestamp_of_last_update: 0,
-        should_aggregate: false,
+pub struct DownstreamConfig {
+    pub address: String,
+    pub port: u16,
+    pub difficulty_config: DownstreamDifficultyConfig,
+}
+
+impl DownstreamConfig {
+    pub fn new(address: String, port: u16, difficulty_config: DownstreamDifficultyConfig) -> Self {
+        Self {
+            address,
+            port,
+            difficulty_config,
+        }
     }
 }
 
 impl ProxyConfig {
+    pub fn new(
+        upstream: UpstreamConfig,
+        downstream: DownstreamConfig,
+        max_supported_version: u16,
+        min_supported_version: u16,
+        min_extranonce2_size: u16,
+    ) -> Self {
+        Self {
+            upstream_address: upstream.address,
+            upstream_port: upstream.port,
+            upstream_authority_pubkey: upstream.authority_pubkey,
+            downstream_address: downstream.address,
+            downstream_port: downstream.port,
+            max_supported_version,
+            min_supported_version,
+            min_extranonce2_size,
+            downstream_difficulty_config: downstream.difficulty_config,
+            upstream_difficulty_config: upstream.difficulty_config,
+        }
+    }
+    
     pub fn apply_env_overrides(mut self) -> Self {
         if let Ok(addr) = env::var("TPROXY_CONFIG_UPSTREAM_ADDRESS") {
             self.upstream_address = addr;
@@ -112,27 +132,6 @@ impl ProxyConfig {
 
         self
     }
-
-    pub fn new(
-        upstream: UpstreamConfig,
-        downstream: DownstreamConfig,
-        max_supported_version: u16,
-        min_supported_version: u16,
-        min_extranonce2_size: u16,
-    ) -> Self {
-        Self {
-            upstream_address: upstream.address,
-            upstream_port: upstream.port,
-            upstream_authority_pubkey: upstream.authority_pubkey,
-            downstream_address: downstream.address,
-            downstream_port: downstream.port,
-            max_supported_version,
-            min_supported_version,
-            min_extranonce2_size,
-            downstream_difficulty_config: downstream.difficulty_config,
-            upstream_difficulty_config: upstream.difficulty_config,
-        }
-    }
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -145,14 +144,6 @@ pub struct DownstreamDifficultyConfig {
     pub submits_since_last_update: u32,
     #[serde(default)]
     pub timestamp_of_last_update: u64,
-}
-
-fn default_min_hashrate() -> f32 {
-    15_000_000.0
-}
-
-fn default_shares_per_minute() -> f32 {
-    10.0
 }
 
 impl DownstreamDifficultyConfig {
@@ -170,7 +161,6 @@ impl DownstreamDifficultyConfig {
         }
     }
 }
-
 impl PartialEq for DownstreamDifficultyConfig {
     fn eq(&self, other: &Self) -> bool {
         other.min_individual_miner_hashrate.round() as u32
@@ -190,14 +180,6 @@ pub struct UpstreamDifficultyConfig {
     pub should_aggregate: bool,
 }
 
-fn default_channel_diff_update_interval() -> u32 {
-    60
-}
-
-fn default_channel_nominal_hashrate() -> f32 {
-    15_000_000.0
-}
-
 impl UpstreamDifficultyConfig {
     pub fn new(
         channel_diff_update_interval: u32,
@@ -214,41 +196,58 @@ impl UpstreamDifficultyConfig {
     }
 }
 
-pub struct UpstreamConfig {
-    pub address: String,
-    pub port: u16,
-    pub authority_pubkey: Secp256k1PublicKey,
-    pub difficulty_config: UpstreamDifficultyConfig,
+fn default_upstream_address() -> String {
+    "127.0.0.1".to_string()
 }
 
-impl UpstreamConfig {
-    pub fn new(
-        address: String,
-        port: u16,
-        authority_pubkey: Secp256k1PublicKey,
-        difficulty_config: UpstreamDifficultyConfig,
-    ) -> Self {
-        Self {
-            address,
-            port,
-            authority_pubkey,
-            difficulty_config,
-        }
+fn default_upstream_port() -> u16 {
+    34254
+}
+
+fn default_upstream_pubkey() -> Secp256k1PublicKey {
+    "9auqWEzQDVyd2oe1JVGFLMLHZtCo2FFqZwtKA5gd9xbuEu7PH72"
+        .parse()
+        .unwrap()
+}
+
+fn default_downstream_address() -> String {
+    "0.0.0.0".to_string()
+}
+
+fn default_downstream_port() -> u16 {
+    34255
+}
+
+fn default_downstream_config() -> DownstreamDifficultyConfig {
+    DownstreamDifficultyConfig {
+        min_individual_miner_hashrate: default_min_hashrate(),
+        shares_per_minute: default_shares_per_minute(),
+        submits_since_last_update: 0,
+        timestamp_of_last_update: 0,
     }
 }
 
-pub struct DownstreamConfig {
-    pub address: String,
-    pub port: u16,
-    pub difficulty_config: DownstreamDifficultyConfig,
+fn default_upstream_config() -> UpstreamDifficultyConfig {
+    UpstreamDifficultyConfig {
+        channel_diff_update_interval: default_channel_diff_update_interval(),
+        channel_nominal_hashrate: default_channel_nominal_hashrate(),
+        timestamp_of_last_update: 0,
+        should_aggregate: false,
+    }
 }
 
-impl DownstreamConfig {
-    pub fn new(address: String, port: u16, difficulty_config: DownstreamDifficultyConfig) -> Self {
-        Self {
-            address,
-            port,
-            difficulty_config,
-        }
-    }
+fn default_min_hashrate() -> f32 {
+    15_000_000.0
+}
+
+fn default_shares_per_minute() -> f32 {
+    10.0
+}
+
+fn default_channel_diff_update_interval() -> u32 {
+    60
+}
+
+fn default_channel_nominal_hashrate() -> f32 {
+    15_000_000.0
 }
